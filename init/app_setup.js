3698467bf3ed072306bdcb8276559c2d8365b5df
ohad partuck <ohadpartuck@gmail.com>
2014-05-02 10:53:43 +0300

    adding the global constatnt module

--- init/app_setup.js
+++ init/app_setup.js
@@ -1,18 +1,22 @@
-var    extend              = require('util')._extend,
-       bodyParser          = require('body-parser');
+var    extend                   = require('util')._extend,
+       bodyParser               = require('body-parser'),
+       global_constants         = require('global_constants');
 
 module.exports = function (app) {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded());
 
-    GLOBAL.MAIN_CONFIG      = require_settings('main');
-    GLOBAL.SANGER_CONFIG    = require_settings('sanger');
+    GLOBAL.MAIN_CONFIG          = require_settings('main');
+    GLOBAL.SANGER_CONFIG        = require_settings('sanger');
+
+
+    GLOBAL.SANGER_CONSTATNTS    = global_constants['sanger']['sanger_constants'];
 };
 
 
 function require_settings(namespace){
-    var defaults            = require('../configuration/' + namespace + '/defaults.json'),
-        by_env              = require('../configuration/' + namespace + '/' + GLOBAL.ENV + '.json');
+    var defaults                = require('../configuration/' + namespace + '/defaults.json'),
+        by_env                  = require('../configuration/' + namespace + '/' + GLOBAL.ENV + '.json');
     return extend(defaults, by_env);
 }
